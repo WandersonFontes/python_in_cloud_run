211c2f0ca89c54d1ccd840f6961eeaabbb1a69ff
Wanderson <wanderson.fontes@gran.com>
2024-08-23 14:56:06 -0300

    feat: small changes in project

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import time
-
-def main():
-    print("Script iniciado no Cloud Run!")
-    time.sleep(5)
-    print("Script finalizado com sucesso!")
-
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+    print("Iniciando projeto no cloud run")
+    return "Script finalizado no Cloud Run!"
 if __name__ == '__main__':
-    main()
+    app.run(host='0.0.0.0', port=8080)
