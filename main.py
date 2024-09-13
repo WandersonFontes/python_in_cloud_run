f60e14d05680f90ee695548cc9692e777cb6858d
Wanderson <wanderson.fontes@gran.com>
2024-09-13 08:43:51 -0300

    feat: add timeout

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from time import sleep
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-    print("Iniciando projeto no cloud run")
+    for _ in range(1,6):
+        sleep(1)
+
     return "Script finalizado no Cloud Run!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
