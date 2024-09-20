72c2cd9184d758ae9c395b935418d55c01814e70
Wanderson <wanderson.fontes@gran.com>
2024-09-20 08:55:06 -0300

    test

--- main.py
+++ main.py
@@ -5,8 +5,5 @@ def main(request) -> None:
     try:
         dados = request.get_json(silent=True)
         return dados, 200
-    except:
-        return f"Error: {request}", 500
-
-if __name__ == "__main__":
-    main(None)
+    except Exception as e:
+        return f"Error: {str(e)}", 500
